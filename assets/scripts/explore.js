c9a6bcb2b183df0ad34e1e2bfecc3d9acd343997
shkimsito <shkim@tuta.io>
2023-05-22 14:00:46 -0700

    wd

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,61 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const selector = document.getElementById("voice-select");
+  const text = document.getElementById("text-to-speak");
+  const img = document.querySelector("img");
+  const synth = window.speechSynthesis;
+
+  function populateVoiceList() {
+    if (typeof speechSynthesis === 'undefined') {
+      return;
+    }
+    
+    const voices = speechSynthesis.getVoices();
+    
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+      
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      selector.appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  
+  const talk = document.querySelector("button");
+  talk.addEventListener("click", function() {
+    
+    if (selector.value == "select") {
+      alert("select a voice");
+    }
+    else if (text.value == "") {
+      alert("enter some text");
+    }
+    else {
+      const utt = new SpeechSynthesisUtterance(text.value);
+      utt.voice = synth.getVoices().filter(function(voice) {
+        return voice.name + '(' + voice.lang + ')' == selector.value;
+      })[0];
+      
+      synth.speak(utt);
+      
+      utt.onstart = event => {
+        img.src = "assets/images/smiling-open.png";
+      };
+      utt.addEventListener('end', event => {
+        img.src = "assets/images/smiling.png";
+      });
+    }
+  });
+
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,37 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const horn       = document.querySelector('#horn-select');
+  const img        = document.querySelector('#expose img');
+  const audio      = document.querySelector('#expose audio');
+  const volume     = document.querySelector('#volume');
+  const icon       = document.querySelector('#volume-controls img');
+  const button     = document.querySelector('#expose button');
+  const jsConfetti = new JSConfetti();
+
+  horn.onchange = () => {
+    img.src = `assets/images/${horn.value}.svg`;
+    img.alt = horn.selectedOptions[0].innerText;
+    audio.src = `assets/audio/${horn.value}.mp3`;
+  };
+
+  volume.oninput = () => {
+    const value = volume.value;
+    audio.volume = value / 100.0;
+    var level = 3;
+    if (value == 0) {
+      level = 0;
+    } else if (value < 33) {
+      level = 1;
+    } else if (value < 67) {
+      level = 2;
+    }
+    icon.src = `assets/icons/volume-level-${level}.svg`;
+    icon.alt = `Volume level ${level}`;
+  };
+
+  button.onclick = () => {
+    audio.play();
+    if (horn.selectedIndex == 3) jsConfetti.addConfetti();
+  };
 }
